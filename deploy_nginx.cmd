02a3703a9c6a33abdca1c6c8ae0974dd5fc7a5a6
Waldyr Felix <waldyrfelix@gmail.com>
2019-10-14 18:59:51 -0300

    token endpoint done

--- deploy_nginx.cmd
+++ deploy_nginx.cmd
@@ -1,4 +1,7 @@
 docker rm -f nginx_lua_lb_server
 docker rmi -f nginx_lua
+
 docker build -t nginx_lua .
+
+docker run -d --name redis_db -p 6379:6379 redis
 docker run -d --name nginx_lua_lb_server -p 80:80 nginx_lua
--- lua/token.lua
+++ lua/token.lua
@@ -1,100 +1,84 @@
-function password_encode(password)
-    local bcrypt = require 'bcrypt'
-    return bcrypt.digest(password, 12)
+local cjson = require "cjson"
+local jwt = require "resty.jwt"
+local redis = require "resty.redis"
+
+local function subdomain()
+    local domain = ngx.var.http_host
+    local index = domain:find('.', 1, true)
+    local subdomain = domain:sub(1, index - 1)
+    return subdomain
 end
 
-function check_password(password, encoded_password)
-    local bcrypt = require 'bcrypt'  
-    return bcrypt.verify(password, encoded_password)
-end
+local function authenticate()
+    -- if ngx.req.get_method() ~= ngx.HTTP_POST then return false end
 
+    ngx.req.read_body() -- explicitly read the req body
+    local data = ngx.req.get_body_data()
+    if not data then return false end
 
-function get_user(username)
-    --- Defaults
+    local obj_data = cjson.decode(data)
+    if not obj_data then return false end
 
-    local access_redis_host =  ngx.var.access_redis_host == ''
-        and '127.0.0.1' or ngx.var.access_redis_host
+    if not obj_data.client_id or not obj_data.client_secret then return false end
 
-    local access_redis_port = ngx.var.access_redis_port == ''
-        and 6379 or ngx.var.access_redis_port
+    local redis_key = string.format("%s:%s:%s", subdomain(), obj_data.client_id,
+                                    obj_data.client_secret)
 
-    local access_user_catalogue = ngx.var.access_user_catalogue == ''
-        and 'nginx_catalogue:users' or ngx.var.access_user_catalogue
+    local redis_conn = redis:new()
+    redis_conn:set_timeouts(100, 100, 100) -- 100ms
 
-    --- 
-
-    local redis = require "nginx.redis"
-    local red = redis:new()
-
-    red:set_timeout(100)
-
-    local ok, err = red:connect(access_redis_host, access_redis_port)
-        if not ok then
-            ngx.log(ngx.ERR, "failed to connect to the redis server: ", err)
-            ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-        return
-    end
-
-    local res, err = red:hget(access_user_catalogue, username)
-
-    if not res then
-        ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-        return
-    end
-
-    -- User is not found
-    if res == ngx.null then
-        return
-    end
-
-    return res
-end
-
-
-function authenticate()
-    -- Test Authentication header is set and with a value
-    local header = ngx.req.get_headers()['Authorization']
-    if header == nil or header:find(" ") == nil then
+    local ok, err = redis_conn:connect(os.getenv("REDIS_HOST"),
+                                       os.getenv("REDIS_PORT"))
+    if not ok then
+        ngx.log(ngx.STDERR, "redis connection problem: ", err)
         return false
     end
 
-    local divider = header:find(' ')
-    if header:sub(0, divider-1) ~= 'Basic' then
-       return false
-    end
+    local props, err = redis_conn:hmget(redis_key, "tenant", "subscriber")
+    redis_conn:close()
 
-    local auth = ngx.decode_base64(header:sub(divider+1))
-    if auth == nil or auth:find(':') == nil then
+    if not props then
+        ngx.log(ngx.ERR, "failed to get redis key: ", redis_key, ", ", err)
         return false
     end
 
-    divider = auth:find(':')
-    local username = auth:sub(0, divider-1)
-    local password = auth:sub(divider+1)
-
-    local res = get_user(username)
-
-    if res == nil then
+    if props[1] == ngx.null or props[2] == ngx.null then
+        ngx.log(ngx.NOTICE, "redis key not found: ", redis_key)
         return false
     end
 
-    if check_password(password, res) then
-        return true
-    end
-
-    return false
+    return {
+        client_id = obj_data.client_id,
+        tenant = props[1],
+        subscriber = props[2]
+    }
 end
 
+local function generate_token(user)
+    local exp_token = 60 * 60
+    local jwt_key = os.getenv("JWT_KEY")
 
--- local user = authenticate()
+    local jwt_token = jwt:sign(jwt_key, {
+        header = {typ = "JWT", alg = "HS256"},
+        payload = {
+            sub = user.subscriber, -- subject
+            iss = user.tenant, -- issuer
+            aud = user.client_id, -- audience
+            iat = os.time(), -- issued at
+            exp = os.time() + exp_token -- expires
+        }
+    })
 
+    return {access_token = jwt_token, expires_in = exp_token}
+end
 
--- if not user then
---    ngx.header.content_type = 'text/plain'
---    ngx.header.www_authenticate = 'Basic realm=""'
---    ngx.status = ngx.HTTP_UNAUTHORIZED
---    ngx.say('401 Access Denied')
--- end
-
-ngx.header.content_type = "application/json"
-ngx.say('{ "access_token": "your access token", "expires": 3600 }')
+local user = authenticate()
+if user then
+    local token = generate_token(user)
+    ngx.header.content_type = "application/json"
+    ngx.say(cjson.encode(token))
+else
+    ngx.header.content_type = 'text/plain'
+    ngx.status = ngx.HTTP_BAD_REQUEST
+    ngx.say("HTTP 400 - Bad request")
+end
